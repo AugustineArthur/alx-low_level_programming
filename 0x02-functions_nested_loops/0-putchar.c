b102fe65d1ffd9ea66260c6eb7046a60eca1956f
Eric Augustine Arthur <augustinearthur07@gmail.com>
2022-07-05 05:18:31 -0700

    My first commit and Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* main - Print the word "_putchar" and a newline
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	char p[] = "_putchar";
+
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
