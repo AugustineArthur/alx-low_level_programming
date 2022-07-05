b0755cf3730e3d7f0a330a3886bf783a8c6ba9db
Eric Augustine Arthur <augustinearthur07@gmail.com>
2022-07-05 05:32:50 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'Z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
