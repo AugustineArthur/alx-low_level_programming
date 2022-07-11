ec95e11d489ca70884139689632d3ab9e9e9f3d8
Eric Augustine Arthur <augustinearthur07@gmail.com>
2022-07-11 07:32:59 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts -  a function that prints a string, followed by a new line, to stdout
- * @str - char array string type
+ * @str: char array string type
  * return - newline after string is printed
  */
 
